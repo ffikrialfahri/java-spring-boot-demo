27313cce2be7de78fedd61b2c6fdd0778bb904b3
Fikri Alfahri <fahrialfikri05@gmail.com>
2025-04-30 20:04:36 +0700

    Start

---
+++ src/main/java/com/enigma/spring_fikri_demo/SpringFikriDemoApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.spring_fikri_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFikriDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFikriDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/spring_fikri_demo/SpringFikriDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.spring_fikri_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFikriDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
